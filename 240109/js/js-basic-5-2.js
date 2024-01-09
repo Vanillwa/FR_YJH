d1e62fe89d3c8b1f0dc381843579e5ef7a41497b
Yoon Jeong Hyeon <yjh30448813@gmail.com>
2024-01-09 17:48:51 +0900

    240109

---
+++ 240109/js/js-basic-5-2.js
@@ -0,0 +1,7 @@
+import "../student"
+
+for(let i of stList){
+    document.write('<p>이름 : '+ i.name + "</p>");
+}
+
+document.write('asdf');
---
+++ 240109/js/student.js
@@ -0,0 +1,6 @@
+let stList = [
+    {name : "이아무개", id:101, hobby : ['수영'], score:{국어:85, 수학:75}},
+    {name : "김아무개", id:102, hobby : ['독서'], score:{국어:81, 수학:71}}
+]
+
+export default stList;
