c7f7495158e5a54ea5bd7e6bd9c1174a2ba152a3
Yoon Jeong Hyeon <yjh30448813@gmail.com>
2024-01-23 14:01:11 +0900

    240123

--- Homework_YJH/js/signin.js
+++ Homework_YJH/js/signin.js
@@ -2,7 +2,10 @@ let userInfo = {
     id : 'test',
     pwd : 'test1234!@'
 }
+console.log('테스트 계정 ▼')
+console.log(userInfo);
 
+// input 태그 focus blur 이벤트 설정
 $('.input').on('focus', function(){
     $(this).parent().addClass('label-up');
 });
@@ -49,14 +52,15 @@ function send(f){
 
     //로그인 검사
     if(id == userInfo.id && pwd == userInfo.pwd){
-        alert('로그인 성공!');
-        return;
+        alert('로그인 성공');
+        location.href='index.html';
     }else if(id == userInfo.id && pwd != userInfo.pwd){
-        alert('비밀번호가 일치하지 않습니다!')
-        $('#pwd').focus();
+        alert('비밀번호가 일치하지 않습니다.')
+        $('#pwd').val('').focus();
         return;
     }else{
-        alert('존재하지 않는 계정입니다!');
+        alert('존재하지 않는 계정입니다.');
+        $('#login-form')[0].reset();
         $('#id').focus();
         return;
     }
@@ -77,4 +81,18 @@ $('#pwd-toggle').on('click', function(){
     }else{
         $('#pwd').prop('type', 'password');
     }
+})
+
+// capslock-alert
+$('#pwd').on('keydown keyup', function(e){
+  if (e.originalEvent.getModifierState('CapsLock')) {
+        $('#capslock-alert').addClass('show')
+    }else{
+        $('#capslock-alert').removeClass('show')
+    }
+})
+
+// pwd input에서 blur시 capslock-alert 제거
+$('#pwd').on('blur', function(){
+    $('#capslock-alert').removeClass('show')
 })
