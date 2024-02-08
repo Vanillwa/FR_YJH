489289823364daa3c4b91fb9f387457a0742310a
Vanillwa <yjh30448813@gmail.com>
2024-02-08 17:45:53 +0900

    240208

---
+++ 240208/ex-1/server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const mysql = require("mysql2");
+const dbConfig = require("./dbConfig");
+const db = mysql.createConnection(dbConfig);
+const models = require("./models");
+
+app.listen(8081, () => {
+    console.log("server on http://localhost:8081/");
+});
+
+app.get("/", (req, res) => {
+    res.render("index")
+});
+
+app.get('/member', async(req, res)=>{
+    let team = req.query.team
+    if(team == undefined || team == ''){
+        await models.Member.findAll().then((data)=>{
+            res.render('member', {data})
+        })
+    }else{
+        await models.Member.findAll({
+            where : {team : team}
+        }).then((data)=>{
+            res.render('member', {data})
+        })
+    }
+})
+
+app.get('/member/view/:id', async(req, res)=>{
+    let {id} = req.params
+    await models.Member.findByPk(id).then((data)=>{
+        res.render('member-view', {data})
+    })
+})
+
+app.get('/member/add', (req, res)=>{
+    res.render('member-add')
+})
+
+app.post('/member/add', async(req, res)=>{
+    await models.Member.create(req.body).then(()=>{
+        res.redirect('../member')
+    })
+})
+
+app.delete('/member/:id', async(req, res)=>{
+    let {id} = req.params
+    await models.Member.destroy({
+        where : {id : id}
+    }).then(()=>{
+        res.redirect('../../member')
+    })
+})
+
+app.get('/member/update/:id', async(req, res)=>{
+    let {id} = req.params
+    await models.Member.findByPk(id).then((data)=>{
+        res.render('member-update', {data})
+    })
+})
+
+app.put('/member/:id', async(req, res)=>{
+    let {id} = req.params
+    await models.Member.update(req.body, {where : {id : id}}).then(()=>{
+        res.redirect('../../member')
+    })
+})
